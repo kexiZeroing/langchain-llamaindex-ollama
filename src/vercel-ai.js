fb6e8424c9267d731f8f781e52cc02db9baf8be6
Kexi Dang <dangkexi@gmail.com>
2024-08-12 10:20:21 +0800

    use ai embedMany

--- src/vercel-ai.js
+++ src/vercel-ai.js
@@ -1,9 +1,14 @@
 // https://github.com/sgomez/ollama-ai-provider
 
-import { streamText } from 'ai'
+import { generateText, streamText, embedMany } from 'ai'
 import { ollama } from 'ollama-ai-provider'
 
 async function main() {
+  // const { text } = await generateText({
+  //   model: ollama('gemma:2b'),
+  //   messages: [{ role: "user", content: 'Invent a new holiday and describe its traditions.' }],
+  // });
+
   const result = await streamText({
     maxTokens: 1024,
     model: ollama('gemma:2b'),
@@ -16,6 +21,32 @@ async function main() {
   }
 
   console.log('Token usage:', await result.usage)
+
+  console.log('Generating embeddings...')
+  generateEmbeddings(`
+    My favorite food is sushi.
+    My hobby is reading.
+    I often go hiking on weekends.
+  `)
 }
 
 main()
+
+const generateEmbeddings = async (value) => {
+  const chunks = generateChunks(value);
+  const embeddingModel = ollama.embedding("nomic-embed-text");
+
+  const { embeddings } = await embedMany({
+    model: embeddingModel,
+    values: chunks,
+  });
+
+  console.log(embeddings.map((e, i) => ({ content: chunks[i], embedding: e })));
+};
+
+const generateChunks = (input) => {
+  return input
+    .trim()
+    .split("\n")
+    .filter((i) => i !== "");
+};
